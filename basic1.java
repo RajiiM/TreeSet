f2a159882aa9d1f72e9f03771e20d891bb54f41c
M.Rajalakshmi <mrajalakshmiofficial@gmail.com>
2021-12-22 15:01:37 +0530

    update1

---
+++ basic1.java
@@ -0,0 +1,20 @@
+import java.util.TreeSet;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		TreeSet ts = new TreeSet();
+		ts.add(10);
+		ts.add(6);
+		ts.add(15);
+		ts.add(4);
+		System.out.println(ts);			
+		System.out.println(ts.size());		
+	}
+}
+------------------------------------
+OUTPUT:
+[4, 6, 10, 15]
+4
+===================================================
+===================================================
