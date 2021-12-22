d8f767a360850eeff342c89eee4003a394905913
M.Rajalakshmi <mrajalakshmiofficial@gmail.com>
2021-12-22 16:53:57 +0530

    Updated

--- foreach.java
+++ foreach.java
@@ -38,3 +38,41 @@ Rajii
 1000
 =========================================================
 =========================================================
+import java.util.TreeSet;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		TreeSet ts = new TreeSet();
+		ts.add("Rajii");
+		ts.add("Abi");
+		ts.add("Gowsy");	
+		System.out.println(ts);
+		
+		for(Object name:ts)
+		{
+			String s = (String)name;//Type Casting			
+			if(s.charAt(0)=='G')
+			{
+			System.out.println(name);
+			}
+		}
+		
+		String frnds[] = {"Rajii", "Abi", "Arjun"};
+		for(String name:frnds)
+		{
+			if(name.charAt(0)=='A')
+			{
+			System.out.println(name);
+			}
+		}
+	}
+}
+-------------------------------------------------
+OUTPUT:
+[Abi, Gowsy, Rajii]
+Gowsy
+Abi
+Arjun
+=============================================
+=============================================
