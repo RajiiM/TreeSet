161107839ea96d2b3502d669704eae64c0e85085
M.Rajalakshmi <mrajalakshmiofficial@gmail.com>
2021-12-22 16:16:46 +0530

    updated2

--- basic1.java
+++ basic1.java
@@ -38,3 +38,35 @@ OUTPUT:
 4
 ====================================================
 ====================================================
+import java.util.TreeSet;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		TreeSet ts = new TreeSet();
+		ts.add("rajii");
+		ts.add("abi");
+		ts.add("gowsy");
+		ts.add("mehra");
+		System.out.println(ts);			
+		System.out.println(ts.size());	
+		System.out.println(ts.contains("abi"));
+		System.out.println(ts.first());
+		System.out.println(ts.last());
+		System.out.println(ts.pollFirst());
+		System.out.println(ts.pollLast());
+		System.out.println(ts.subSet("abi", "rajii"));
+	}
+}
+----------------------
+Output:
+[abi, gowsy, mehra, rajii]
+4
+true
+abi
+rajii
+abi
+rajii
+[gowsy, mehra]
+===========================================
+===========================================
